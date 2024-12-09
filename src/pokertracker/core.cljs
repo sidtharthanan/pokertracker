37ecf85d91d17b1fe8673d6cfb8826a08f039322
sid <sidthunagarajan@gmail.com>
2024-12-09 23:21:09 +0530

    add example from official docs

--- src/pokertracker/core.cljs
+++ src/pokertracker/core.cljs
@@ -1,19 +1,45 @@
 (ns pokertracker.core
-    (:require
-      [reagent.core :as r]
-      [reagent.dom :as d]))
+  (:require [reagent.core :as r]
+            [reagent.dom :as rdom]
+            [clojure.string :as str]))
 
 ;; -------------------------
 ;; Views
 
+(defonce timer (r/atom (js/Date.)))
+
+(defonce time-color (r/atom "#f34"))
+
+(defonce time-updater (js/setInterval
+                       #(reset! timer (js/Date.)) 1000))
+
+(defn greeting [message]
+  [:h1 message])
+
+(defn clock []
+  (let [time-str (-> @timer .toTimeString (str/split " ") first)]
+    [:div.example-clock
+     {:style {:color @time-color}}
+     time-str]))
+
+(defn color-input []
+  [:div.color-input
+   "Time color: "
+   [:input {:type "text"
+            :value @time-color
+            :on-change #(reset! time-color (-> % .-target .-value))}]])
+
 (defn home-page []
-  [:div [:h2 "Welcome to Reagent"]])
+  [:div
+   [greeting "Hello world, it is now"]
+   [clock]
+   [color-input]])
 
 ;; -------------------------
 ;; Initialize app
 
 (defn mount-root []
-  (d/render [home-page] (.getElementById js/document "app")))
+  (rdom/render [home-page] (js/document.getElementById "app")))
 
 (defn ^:export init! []
   (mount-root))
