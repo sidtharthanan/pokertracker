c617e7aef1be4e725d40dfb03e3eb8e31195cd5c
sid <sidthunagarajan@gmail.com>
2024-12-11 19:27:59 +0530

    Add table for chip management

---
+++ src/pokertracker/configs.cljs
@@ -0,0 +1,12 @@
+(ns pokertracker.configs)
+
+(def ^:export default-state {:chipset [{:denom 1 :color "white" :count 40  :per-player 5}
+                                       {:denom 5 :color "red" :count 40  :per-player 4}
+                                       {:denom 10 :color "green" :count 40  :per-player 4}
+                                       {:denom 25 :color "blue" :count 40  :per-player 3}
+                                       {:denom 50 :color "black" :count 40  :per-player 2}]
+                             
+                             :game-duration-secs (* 2 60 60)
+                             :no-of-players 4
+                             :start-blind 2
+                             :blind-multiplier 1.5})
--- src/pokertracker/core.cljs
+++ src/pokertracker/core.cljs
@@ -1,14 +1,27 @@
 (ns pokertracker.core
   (:require [reagent.core :as r]
             [reagent.dom :as rdom]
-            [clojure.string :as str]))
+            [clojure.string :as str]
+            [pokertracker.configs :as defaults]))
+
+;; -------------------------
+
+(defonce gbl-state (r/atom (or
+                            (.getItem (.-localStorage js/window) :gbl-state)
+                            defaults/default-state)))
+
+(println @gbl-state)
+
+;; -------------------------
 
 ;; -------------------------
 ;; Views
 
-(defonce timer (r/atom (js/Date.)))
+(defonce timer
+  (r/atom (js/Date.)))
 
-(defonce time-color (r/atom "#f34"))
+(defonce time-color (r/atom
+                     (.getItem (.-localStorage js/window) :color)))
 
 (defonce time-updater (js/setInterval
                        #(reset! timer (js/Date.)) 1000))
@@ -18,7 +31,7 @@
 
 (defn clock []
   (let [time-str (-> @timer .toTimeString (str/split " ") first)]
-    [:div.example-clock
+    [:div#id1001.example-clock>p>span
      {:style {:color @time-color}}
      time-str]))
 
@@ -27,13 +40,59 @@
    "Time color: "
    [:input {:type "text"
             :value @time-color
-            :on-change #(reset! time-color (-> % .-target .-value))}]])
+            :on-change (fn [element]
+                         (let [new-color (-> element (.-target) (.-value))]
+                           (.setItem (.-localStorage js/window) :color new-color)
+                           (reset! time-color new-color)))}]])
 
 (defn home-page []
-  [:div
-   [greeting "Hello world, it is now"]
-   [clock]
-   [color-input]])
+  (let [{:keys [chipset no-of-players]} @gbl-state
+        tdata (map (fn [{:keys [denom color count per-player]}]
+                     {:chip denom
+                      :color color
+                      :total-qty count
+                      :total-val (* denom count)
+                      :qty-per-player per-player
+                      :val-per-player (* denom per-player)
+                      :qty-used (* no-of-players per-player)
+                      :qty-left (- count (* no-of-players per-player))
+                      :val-left (* denom (- count (* no-of-players per-player)))}) chipset)]
+    [:div
+     [greeting "Hello world, it is now. Yeah"]
+     [clock]
+     [color-input]
+     [:table {:style {:width 800 :border "1px solid black"}}
+      [:thead
+       [:tr
+        [:th {:style {:border "1px solid black"}} "Chip"]
+        [:th {:style {:border "1px solid black"}} "Total Qty"]
+        [:th {:style {:border "1px solid black"}} "Total Value"]
+        [:th {:style {:border "1px solid black"}} "Qty / Player"]
+        [:th {:style {:border "1px solid black"}} "Value / Player"]
+        [:th {:style {:border "1px solid black"}} "Qty Used"]
+        [:th {:style {:border "1px solid black"}} "Qty Left"]
+        [:th {:style {:border "1px solid black"}} "Value Left"]]]
+      [:tbody
+       (map (fn [{:keys [:chip :total-qty :total-val :qty-per-player :val-per-player :qty-used :qty-left :val-left]}]
+              [:tr {:key chip}
+               [:td {:style {:border "1px solid black"}} chip]
+               [:td {:style {:border "1px solid black"}} total-qty]
+               [:td {:style {:border "1px solid black"}} total-val]
+               [:td {:style {:border "1px solid black"}} qty-per-player]
+               [:td {:style {:border "1px solid black"}} val-per-player]
+               [:td {:style {:border "1px solid black"}} qty-used]
+               [:td {:style {:border "1px solid black"}} qty-left]
+               [:td {:style {:border "1px solid black"}} val-left]]) tdata)]
+      [:tfoot
+       [:tr
+        [:td {:style {:border "1px solid black"}} "Total"]
+        [:td {:style {:border "1px solid black"}} (reduce + (map :total-qty tdata))]
+        [:td {:style {:border "1px solid black"}} (reduce + (map :total-val tdata))]
+        [:td {:style {:border "1px solid black"}} (reduce + (map :qty-per-player tdata))]
+        [:td {:style {:border "1px solid black"}} (reduce + (map :val-per-player tdata))]
+        [:td {:style {:border "1px solid black"}} (reduce + (map :qty-used tdata))]
+        [:td {:style {:border "1px solid black"}} (reduce + (map :qty-left tdata))]
+        [:td {:style {:border "1px solid black"}} (reduce + (map :val-left tdata))]]]]]))
 
 ;; -------------------------
 ;; Initialize app
